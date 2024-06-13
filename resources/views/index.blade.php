71f5d41f5746b4b9bf22ad5434f124a389647fb2
Pascal Wiersma <pascal@psww.nl>
2024-06-13 17:08:02 +0200

    chore: Update dependencies and configuration files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Inkomsten
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Uitgaven
+                </div>
+                <div class="card-body">
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
